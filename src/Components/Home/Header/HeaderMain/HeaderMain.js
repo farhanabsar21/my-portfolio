85a8b0ae3e96bd5cf0705f825b0338d5120c4a01
Farhan21 <farhanabsar21@gmail.com>
2021-08-01 16:39:38 +0600

    updated

--- src/Components/Home/Header/HeaderMain/HeaderMain.js
+++ src/Components/Home/Header/HeaderMain/HeaderMain.js
@@ -11,22 +11,23 @@ const HeaderMain = () => {
         <main className="Header-main">
             <div className="container h-100">
                 <div className="row h-100">
-                    <div className="col-md-6 d-flex align-items-center h-100">
+                    <div className="col-md-7 d-flex align-items-center h-100">
                         <section>
-                            <h1>HI, I'M <br></br>FARHAN ABSAR <br></br> TAFADER</h1>
-                            <p className="mt-2">
-                                <Typewriter options={{loop: true, autoStart: true,}} onInit={typewriter =>{
-                                    typewriter.typeString("I'm a web developer, web designer, ux/ui designer and creator. I always Search for Excellence and always dream to be the light of tomorrow.")
+                            <h1>hi, i'm <br /> farhan absar tafader</h1>
+                            <p className="mt-2"> 
+                                I'm a <Typewriter options={{loop: true, autoStart: true,}} onInit={typewriter =>{
+                                    typewriter.typeString("Web developer, Web designer, UX/UI designer.")
                                     .pauseFor(500)
                                     .deleteAll()
                                     .start()
                                 }}></Typewriter>
+                                I always Search for Excellence and always dream to be the light of tomorrow.
                             </p>
                             <button className="mt-2"><a href={pdfFile} download><FontAwesomeIcon icon={faDownload} color="black" /> Download My Resume</a></button>
                         </section>
                     </div>
-                    <div className="col-md-6 d-flex align-items-end justify-content-end">
-                        <section>
+                    <div className="col-md-5 d-flex align-items-end justify-content-end">
+                        <section className="social-links">
                             <a href="https://www.facebook.com/farhanabsartafader/"><FontAwesomeIcon className="mx-2" icon={faFacebook} color="white" size="2x"/></a>
                             <a href="https://github.com/farhanabsar21"><FontAwesomeIcon className="mx-2" icon={faGithub} color="white" size="2x"/></a>
                             <a href="https://www.linkedin.com/in/farhanabsartafader/"><FontAwesomeIcon className="mx-2" icon={faLinkedin} color="white" size="2x"/></a>
